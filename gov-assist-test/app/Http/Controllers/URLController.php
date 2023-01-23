df3e14a947c428227b15c34607b52d20ca86d0e8
Malik Faizan Zafar <malikfaizanzafar1993@gmail.com>
2023-01-23 23:02:14 +0500

    model, controller , migration for url model

---
+++ gov-assist-test/app/Http/Controllers/URLController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\URL;
+use Illuminate\Http\Request;
+
+class URLController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\URL  $uRL
+     * @return \Illuminate\Http\Response
+     */
+    public function show(URL $uRL)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\URL  $uRL
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(URL $uRL)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\URL  $uRL
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, URL $uRL)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\URL  $uRL
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(URL $uRL)
+    {
+        //
+    }
+}
